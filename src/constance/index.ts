bcf70f359dd86217b50dcaaabed1e7d02066e8cd
Melu-Sahu <melusahu0143.s@gmail.com>
2024-03-18 23:43:19 +0530

    commented chat functionality in constent folder's index file.

--- src/constance/index.ts
+++ src/constance/index.ts
@@ -1,72 +1,72 @@
 export const sidebarLinks = [
-    {
-      imgURL: "/assets/icons/home.svg",
-      route: "/",
-      label: "Home",
-    },
-    {
-      imgURL: "/assets/icons/wallpaper.svg",
-      route: "/explore",
-      label: "Explore",
-    },
-    {
-      imgURL: "/assets/icons/people.svg",
-      route: "/all-users",
-      label: "People",
-    },
-    {
-      imgURL: "/assets/icons/bookmark.svg",
-      route: "/saved",
-      label: "Saved",
-    },
-    {
-      imgURL: "/assets/icons/gallery-add.svg",
-      route: "/create-post",
-      label: "Create Post",
-    },
-    {
-      imgURL: "/assets/icons/chat.svg",
-      route: "/chat",
-      label: "Chat",
-    },
-    
-  ];
-  
-  export const bottombarLinks = [
-    {
-      imgURL: "/assets/icons/home.svg",
-      route: "/",
-      label: "Home",
-    },
-    {
-      imgURL: "/assets/icons/wallpaper.svg",
-      route: "/explore",
-      label: "Explore",
-    },
-    {
-      imgURL: "/assets/icons/bookmark.svg",
-      route: "/saved",
-      label: "Saved",
-    },
-    {
-      imgURL: "/assets/icons/gallery-add.svg",
-      route: "/create-post",
-      label: "Create",
-    },
-  ];
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/all-users",
+    label: "People",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create Post",
+  },
+  // {
+  //   imgURL: "/assets/icons/chat.svg",
+  //   route: "/chat",
+  //   label: "Chat",
+  // },
+
+];
+
+export const bottombarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Saved",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Create",
+  },
+];
 
 
-  export const profileTabs = [
-    {
-      imageUrl: "/assets/icons/posts.svg",
-      label: "Posts"
-    },
-    {
-      imageUrl: "/assets/icons/add-post.svg",
-      label: "Reels"
-    },
-    {
-      imageUrl: "/assets/icons/add-post.svg",
-      label: "Reels"
-    },
-  ]
+export const profileTabs = [
+  {
+    imageUrl: "/assets/icons/posts.svg",
+    label: "Posts"
+  },
+  {
+    imageUrl: "/assets/icons/add-post.svg",
+    label: "Reels"
+  },
+  {
+    imageUrl: "/assets/icons/add-post.svg",
+    label: "Reels"
+  },
+]
